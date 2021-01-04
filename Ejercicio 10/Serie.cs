d45653f2076e3121b86f003df5ac770b5ffd24db
Locryn <sergiomartinezclavero@hotmail.com>
2021-01-04 20:22:15 +0100

    no message

---
+++ Ejercicio 10/Serie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Unidad9
+{
+    class Serie
+    {
+        public string titulo { get; set; }
+        public int NumTemp { get; set; } = 3;
+
+        public bool entregado { get; set; } = false;
+
+        public string genero { get; set; }
+
+        public string creador { get; set; }
+
+        public Serie()
+        {
+
+        }
+
+        public Serie(string titulo, string creador)
+        {
+            this.titulo = titulo;
+            this.creador = creador;
+      
+        }
+
+        public Serie(string titulo, int NumTemp, string genero, string creador)
+        {
+            this.titulo = titulo;
+            this.NumTemp = NumTemp;
+            this.genero = genero;
+            this.creador = creador;
+            
+        }
+
+
+    }
+}
--- Ejercicio 9/Electrodomestico.cs
+++ Ejercicio 9/Electrodomestico.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Proyecto_Unidad9.Ejercicio_9
+namespace Proyecto_Unidad9
 {
     class Electrodomestico
     {
+        public int precio_base { get; set; } = 100;
+        public string color { get; set; } = "blanco";
+        public char consumo_Elec { get; set; } = 'F';
+        public int peso { get; set; } = 5;
+
+        public Electrodomestico()
+        {
+
+        }
+
+        public Electrodomestico(int precio_base, int peso)
+        {
+            this.precio_base = precio_base;
+            this.peso = peso;
+        }
+
+        public Electrodomestico(int precio_base, string color, char consumo_Elec, int peso)
+        {
+            this.precio_base = precio_base;
+            this.color = color;
+            this.consumo_Elec = consumo_Elec;
+            this.peso = peso;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Precio: {0}, Color: {1}, Consumo: {2}, Peso: {3}",precio_base,color,consumo_Elec,peso);
+        }
 
 
     }
